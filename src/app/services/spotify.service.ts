0ede5c1314f3446576f2dc0af6aebc5b9e276af5
Aaron Velasco <tiquismiquisss@gmail.com>
2019-04-09 15:32:12 +0200

    unificar peticiones de servicio spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,21 +11,20 @@ const TOKEN = 'Bearer BQC8LSR0zd_RjuBC4Qtec0X-yFXL1hchVxxThdJULlWY9FI__skCzr-DUm
 export class SpotifyService {
   constructor(private http: HttpClient) { }
 
-  getNewReleases() {
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
       Authorization: TOKEN
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers})
-      .pipe(map((data: any) => data.albums.items));
+    return this.http.get(url, {headers});
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases').pipe(map((data: any) => data.albums.items));
   }
 
   getArtista(termino: string) {
-    const headers = new HttpHeaders({
-      Authorization: TOKEN
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers})
-      .pipe(map((data: any) => data.artists.items));
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(map((data: any) => data.artists.items));
   }
 }
