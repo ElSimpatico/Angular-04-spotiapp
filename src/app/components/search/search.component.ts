aeffae1c93a0609c726790ebbcdd8aaa3961ec79
Aaron Velasco <tiquismiquisss@gmail.com>
2019-04-09 13:18:16 +0200

    componente de busqueda

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  artistas: any[] = [];
+  constructor(private spotify: SpotifyService) { }
 
   ngOnInit() {
+  }
+
+  buscar(termino: string) {
+    console.log(termino);
+    this.spotify.getArtista(termino)
+      .subscribe((data: any) => {
+        console.log(data.artists.items);
+        this.artistas = data.artists.items;
+      });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+const TOKEN = 'Bearer BQBMSTAG60r3yaalIcu88QFqMjawgA3Vi2Qe3VrbXva9u1s2zhdgab8BcrMG_E7pTV-w1ppagUTiGTEWU3Q';
+
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
-
   constructor(private http: HttpClient) { }
 
   getNewReleases() {
     const headers = new HttpHeaders({
-      // tslint:disable-next-line: max-line-length
-      Authorization: 'Bearer BQAT4zpRchx-SqXAtmRFP0Y7npAObmXcbaS_a-xNV8GqA6nSL_FiRWRjxi7m8BQfIUmiwNE6tIhF981c4-o99h5U4BTTAgIe_wniq_3QaG8pr2NoY53RIZZuCqcfP73pdZ00kqE44KyBFQ'
+      Authorization: TOKEN
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+  }
+
+  getArtista(termino: string) {
+    const headers = new HttpHeaders({
+      Authorization: TOKEN
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
   }
 }
