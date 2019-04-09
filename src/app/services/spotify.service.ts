49936810a887f7a7867c4083fe5a5b14db567178
Aaron Velasco <tiquismiquisss@gmail.com>
2019-04-09 12:03:20 +0200

    peticion rest (HttpClient, HttpHeaders)

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) { }
+
+  getNewReleases() {
+    const headers = new HttpHeaders({
+      // tslint:disable-next-line: max-line-length
+      Authorization: 'Bearer BQAT4zpRchx-SqXAtmRFP0Y7npAObmXcbaS_a-xNV8GqA6nSL_FiRWRjxi7m8BQfIUmiwNE6tIhF981c4-o99h5U4BTTAgIe_wniq_3QaG8pr2NoY53RIZZuCqcfP73pdZ00kqE44KyBFQ'
+    });
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers})
+    .subscribe( data => console.log(data));
+  }
+}
